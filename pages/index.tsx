cf516e46862ccc78cbd34b94a3d5b276ba9c3fc6
Sérvulo Sávio Medeiros <servulosavio@gmail.com>
2023-05-03 01:18:43 -0300

    Criação do header da página homeNoAuth - Header não autenticado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,24 @@
 import Head from "next/head";
-import styles from '../styles/HomeNoAuth.module.scss';
-
+import styles from "../styles/HomeNoAuth.module.scss";
+import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 
 const HomeNoAuth = () => {
-    return (
-        <>
-            <Head>
-                <title>Home</title>
-            </Head>
-            <main></main>
-        </>
-    );
-}
+  return (
+    <>
+      <Head>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta
+          name="description"
+          content="Tenha acesso aos melhores conteúdos de programação de uma forma simples e fácil!"
+        />
+      </Head>
+      <main>
+        <HeaderNoAuth />
+      </main>
+    </>
+  );
+};
 
 export default HomeNoAuth;
