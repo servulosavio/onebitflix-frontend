8d3fcf963672b07d7a434ad4f4c21764a499c28d
Sérvulo Sávio Medeiros <servulosavio@gmail.com>
2023-05-04 20:53:35 -0300

    Finalização da estrutura dos slides - Slide Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,17 @@ import styles from "../styles/HomeNoAuth.module.scss";
 import HeaderNoAuth from "@/src/components/homeNoAuth/headerNoAuth";
 import PresentationSection from "@/src/components/homeNoAuth/presentationSection";
 import CardsSection from "@/src/components/homeNoAuth/cardsSection";
+import SlideSection from "@/src/components/homeNoAuth/slideSection";
+import { GetStaticProps } from "next";
+import courseService, { CourseType } from "@/src/services/courseService";
+import { ReactNode } from "react";
 
-const HomeNoAuth = () => {
+interface IndexPageProps {
+  chirldren?: ReactNode;
+  course: CourseType[];
+}
+
+const HomeNoAuth = ({ course }: IndexPageProps) => {
   return (
     <>
       <Head>
@@ -23,9 +32,20 @@ const HomeNoAuth = () => {
         </div>
 
         <CardsSection />
+        <SlideSection newestCourses={course} />
       </main>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await courseService.getNewestCourses();
+  return {
+    props: {
+      course: res.data,
+    },
+    revalidate: 3600 * 24,
+  };
 };
 
 export default HomeNoAuth;
---
+++ src/components/homeNoAuth/slideSection/index.tsx
@@ -0,0 +1,27 @@
+import { CourseType } from "@/src/services/courseService";
+import styles from "./styles.module.scss";
+import { Button, Container } from "reactstrap";
+import SlideComponent from "../../common/slideComponent";
+import Link from "next/link";
+
+interface props {
+  newestCourses: CourseType[];
+}
+
+const SlideSection = function ({ newestCourses }: props) {
+  return (
+    <>
+      <Container>
+        <p className={styles.sectionTitle}>AULAS JÁ DISPONÍVEIS</p>
+        <SlideComponent course={newestCourses} />
+        <Link href="/register">
+          <Button outline color="light" className={styles.SlideSectionBtn}>
+            Se cadastre para acessar!
+          </Button>
+        </Link>
+      </Container>
+    </>
+  );
+};
+
+export default SlideSection;
