d2d78efd01277685dd5bec1853c8a4917a2b2311
Sérvulo Sávio Medeiros <servulosavio@gmail.com>
2023-05-04 22:33:24 -0300

    Página homeNoAuth finalizada. Toda responsividade e também as seções funcionais. Conexão com o backend e utilização do getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import SlideSection from "@/src/components/homeNoAuth/slideSection";
 import { GetStaticProps } from "next";
 import courseService, { CourseType } from "@/src/services/courseService";
 import { ReactNode } from "react";
+import Footer from "@/src/components/common/footer";
 
 interface IndexPageProps {
   chirldren?: ReactNode;
@@ -33,6 +34,7 @@ const HomeNoAuth = ({ course }: IndexPageProps) => {
 
         <CardsSection />
         <SlideSection newestCourses={course} />
+        <Footer />
       </main>
     </>
   );
