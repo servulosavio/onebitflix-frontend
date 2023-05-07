dc9b5bd6214bf45561fc39c55266ccb2807a1d57
Sérvulo Sávio Medeiros <servulosavio@gmail.com>
2023-05-06 21:36:18 -0300

    Finalização do header genérico, do seu estilo visual e da sua responsividade

---
+++ pages/register.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styles from "../styles/registerLogin.module.scss";
+import HeaderGeneric from "@/src/components/common/headerGeneric";
+
+const Register = function () {
+  return (
+    <>
+      <Head>
+        <title>Onebitflix - Registro</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+      </Head>
+      <main>
+        <HeaderGeneric
+          logoUrl="/"
+          btnUrl="/login"
+          btnContent="Quero fazer login"
+        />
+      </main>
+    </>
+  );
+};
+
+export default Register;
