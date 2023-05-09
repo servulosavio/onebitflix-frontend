7bbbb4ab8a5e49b4946c5f16018a3ee2de37a706
Sérvulo Sávio Medeiros <servulosavio@gmail.com>
2023-05-08 22:11:05 -0300

    Configuração finalizada da seção de destaque. Feita a conexão com o backend, criando um método para o serviço de curso. Criando a sua responsividade - Seção destaque

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -2,31 +2,49 @@ import { error } from "console";
 import api from "./api";
 
 export type EpisodeType = {
-    id: number;
-    name: string;
-    synopsis: string;
-    order: number;
-    videoUrl: string;
-    secondsLong: number;
-}
+  id: number;
+  name: string;
+  synopsis: string;
+  order: number;
+  videoUrl: string;
+  secondsLong: number;
+};
 
 export type CourseType = {
-    id: number;
-    name: string;
-    thumbnailUrl: string;
-    synopsis: string;
-    episodes?: EpisodeType[];
+  id: number;
+  name: string;
+  thumbnailUrl: string;
+  synopsis: string;
+  episodes?: EpisodeType[];
 };
 
 const courseService = {
-    getNewestCourses: async () => {
-        const res = await api.get("/courses/newest").catch((error) => {
-            console.log(error.response.data.message);
-            return error.response;
-        })
+  getNewestCourses: async () => {
+    const res = await api.get("/courses/newest").catch((error) => {
+      console.log(error.response.data.message);
+      return error.response;
+    });
 
-        return res;
-    },
-}
+    return res;
+  },
 
-export default courseService
+  getFeaturedCourses: async () => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .get("/courses/featured", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+
+        return error.response;
+      });
+
+    return res;
+  },
+};
+
+export default courseService;
