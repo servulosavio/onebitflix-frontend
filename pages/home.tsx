a2d56bbede573f2adb27fe828827ab27e4b8bf57
Sérvulo Sávio Medeiros <servulosavio@gmail.com>
2023-05-08 22:55:37 -0300

    Criação dos elementos da seção de lançamento. Do seu estilo visual (Título) - Seção lançamento

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,4 +1,5 @@
 import FeaturedSection from "@/src/components/homeAuth/featuredSection";
+import NewestCategory from "@/src/components/homeAuth/newestCategory";
 import Head from "next/head";
 
 const HomeAuth = function () {
@@ -10,6 +11,7 @@ const HomeAuth = function () {
       </Head>
       <main>
         <FeaturedSection />
+        <NewestCategory />
       </main>
     </>
   );
