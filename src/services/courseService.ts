d39d82663cb325c2f2290bf515fc592acd12080b
Sérvulo Sávio Medeiros <servulosavio@gmail.com>
2023-05-11 21:23:42 -0300

    Criação do método de pegar o objeto de um curso, comunicação com o front - Página do curso

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -93,6 +93,36 @@ const courseService = {
     return res;
   },
 
+  like: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .post("/likes",
+        { courseId },
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+      .catch((error) => {
+        return error.response;
+      });
+
+    return res;
+  },
+
+  removeLike: async (courseId: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .delete(`/likes/${courseId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .catch((error) => {
+        return error.response;
+      });
+
+    return res;
+  },
+
   getSearch: async (name: string) => {
     const token = sessionStorage.getItem("onebitflix-token");
 
@@ -106,7 +136,25 @@ const courseService = {
 
     return res;
 
-  }
+  },
+
+
+  getEpisodes: async (id: number | string) => {
+    const token = sessionStorage.getItem("onebitflix-token");
+
+    const res = await api
+      .get(`/courses/${id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .catch((error) => {
+        return error.response;
+      });
+
+    return res;
+
+  },
+
+
 };
 
 export default courseService;
